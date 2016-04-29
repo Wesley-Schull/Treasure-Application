9e98e433ba2ff2fba73032d17afef6012b66c47e
Wesley Schull <wcschull@bsu.edu>
2016-04-28 22:43:34 -0400

    Setup first scene of GUI

--- src/Application/Controller.java
+++ src/Application/Controller.java
@@ -1,4 +1,16 @@
 package Application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
+    public Button submitBalanceButton;
+    public Label testLabel;
+    public TextField balanceGoldTextField;
+    public TextField balanceSilverTextField;
+    public TextField balanceCopperTextField;
+    @FXML
+    public void submitBalanceButtonPressed(){
+        testLabel.setText("Button Pushed");
+    }
 }
