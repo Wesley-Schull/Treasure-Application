fcd9d543dea9bde6a7dfa83a3e896578b12fbd09
Wesley Schull <wcschull@bsu.edu>
2016-04-29 03:55:07 -0400

    Improve GUI (Update Balance button no longer updates Balance and Price; each will update their own labels)

--- src/Application/Controller.java
+++ src/Application/Controller.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class Controller {
-    private Shop shop = new Shop();
+    Shop shop = new Shop();
     @FXML Label currentBalanceLabel, currentPriceLabel;
     @FXML TextField playerGoldTextField, itemGoldTextField, playerSilverTextField, itemSilverTextField, playerCopperTextField, itemCopperTextField;
     @FXML Button submitPlayerBalanceButton, submitItemPriceButton, purchaseItemButton;
@@ -12,14 +12,14 @@ public class Controller {
         shop.playerWallet = new Wallet(Integer.parseInt(playerGoldTextField.getText()),
                 Integer.parseInt(playerSilverTextField.getText()),
                 Integer.parseInt(playerCopperTextField.getText()));
-        displayWallets();
+        currentBalanceLabel.setText(displayThisWallet(shop.playerWallet));
         purchaseButtonVisibilityCheck();
     }
     public void submitItemPriceButtonClicked() {
         shop.priceOfObject = new Wallet(Integer.parseInt(itemGoldTextField.getText()),
                 Integer.parseInt(itemSilverTextField.getText()),
                 Integer.parseInt(itemCopperTextField.getText()));
-        displayWallets();
+        currentPriceLabel.setText(displayThisWallet(shop.priceOfObject));
         purchaseButtonVisibilityCheck();
     }
     public void purchaseItemButtonClicked() {
@@ -27,16 +27,18 @@ public class Controller {
         currentBalanceLabel.setText(shop.playerWallet.toString());
         purchaseButtonVisibilityCheck();
     }
-    private void displayWallets() {
-        currentBalanceLabel.setText(shop.playerWallet.toString());
-        currentPriceLabel.setText(shop.priceOfObject.toString());
-    }
-    private void purchaseButtonVisibilityCheck() {
+    public void purchaseButtonVisibilityCheck() {
         if (isPurchasable())
             purchaseItemButton.setVisible(true);
-        purchaseItemButton.setVisible(false);
+        else
+            purchaseItemButton.setVisible(false);
     }
-    private boolean isPurchasable() {
-        return shop.playerWallet.toCopper() >= shop.priceOfObject.toCopper();
+    public boolean isPurchasable() {
+        if (shop.playerWallet.toCopper() >= shop.priceOfObject.toCopper())
+            return true;
+        return false;
+    }
+    public String displayThisWallet(Wallet thisWallet) {
+        return thisWallet.toString();
     }
 }
