e26eab342c540d5b0ba2739c7553e390a348d673
Wesley Schull <wcschull@bsu.edu>
2016-04-28 23:13:24 -0400

    Test button functionality

--- src/Application/Controller.java
+++ src/Application/Controller.java
@@ -1,14 +1,18 @@
 package Application;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class Controller {
     public Button submitBalanceButton;
     public Label testLabel;
-    public TextField balanceGoldTextField;
-    public TextField balanceSilverTextField;
-    public TextField balanceCopperTextField;
+    public TextField balanceGoldTextField, balanceCopperTextField, balanceSilverTextField;
+    GridPane mainGridPane, setBalanceGridPane, chargeGridPane, creditGridPane;
+    Stage stage;
+    Scene mainScene, setBalanceScene, chargeScene, creditScene;
     @FXML
     public void submitBalanceButtonPressed(){
         testLabel.setText("Button Pushed");
